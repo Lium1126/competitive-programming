0933a9918cfe039d8adf1e386477a14d8c442f41
Lium1126 <yosi.4sya@gmail.com>
2025-02-22 22:42:08 +0900

    ABC394

---
+++ ABC394/b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <stdio.h>
+#include <math.h>
+#include <atcoder/all>
+
+#define ll long long
+#define rep(i, n)     for (ll i = 0;i < n;i++)
+#define rrep(i, n)    for (ll i = n - 1;i >= 0;i--)
+#define irep(i, n)    for (ll i = 1;i <= n;i++)
+#define rirep(i, n)   for (ll i = n;i >= 1;i--)
+#define srep(i, s, n) for (ll i = s;i <= n;i++)
+
+using namespace std;
+using namespace atcoder;
+typedef pair<ll, ll> P;
+
+int main() {
+	ll n;
+	cin >> n;
+
+	vector<string> s(n);
+	vector<P> v(n);
+	rep(i, n) {
+		cin >> s[i];
+		v[i] = {s[i].length(), i};
+	}
+
+	sort(v.begin(), v.end());
+
+	for (auto p : v) {
+		cout << s[p.second];
+	}
+	cout << endl;
+
+	return 0;
+}
